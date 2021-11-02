9aeaeb4616351f4096f352f37d8665776a6dfe5c
iclosed <iclosed@163.com>
2021-11-02 10:38:45 +0800

    build bash

--- build.bat
+++ build.bat
@@ -1,8 +1,18 @@
 @echo off
+set fd=build
 cmake --version
-rd build /s /q
-md build
-cmake -S src -B build
-cmake --build build/
+if exist %fd% (
+	echo ### Delete old build files
+    rd %fd% /s /q
+)
+md %fd%
+echo #
+echo ##
+echo ### CMake from source code...
+cmake -S src -B %fd%
+echo #
+echo ##
+echo ### Building from make files...
+cmake --build %fd%/
 
 pause
